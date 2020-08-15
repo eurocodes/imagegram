810af5d72b1ad95f57b7fede153c283536182f66
eurocodes <emmanuelumeoke@gmail.com>
2020-08-15 05:37:12 +0100

    Enabled upload file

---
+++ imagegram/src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div className='title'>
+            <h1>Imagegram</h1>
+            <h2>Your Pictures</h2>
+            <p>Lorem ipsum dolor sit amet, cosectetur adipiscing elit.</p>
+        </div>
+    )
+}
+
+export default Title;
