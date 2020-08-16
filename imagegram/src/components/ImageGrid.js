26302b74b6c51a0bb4e353f9446d1e57a8f147d8
eurocodes <emmanuelumeoke@gmail.com>
2020-08-16 10:02:08 +0100

    added framer-motion

--- imagegram/src/components/ImageGrid.js
+++ imagegram/src/components/ImageGrid.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion'
 import useFireStore from '../hooks/UseFireStore';
 
 const ImageGrid = ({ setSelectedImg }) => {
@@ -8,11 +9,17 @@ const ImageGrid = ({ setSelectedImg }) => {
     return (
         <div className='img-grid'>
             {docs && docs.map(doc => (
-                <div className='img-wrap' key={doc.id}
+                <motion.div className='img-wrap' key={doc.id}
+                    layout
+                    whileHover={{ opacity: 1 }}
                     onClick={() => setSelectedImg(doc.url)}
                 >
-                    <img src={doc.url} alt='Imagram Image' />
-                </div>
+                    <motion.img src={doc.url} alt='Imagram Image'
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 1 }}
+                    />
+                </motion.div>
             ))}
         </div>
     )
